f7282a59cb60af18c3d7cb21b825733cc43f7d7b
armanfar <Ramin.Armanfar@msg.group>
2019-12-09 11:54:34 +0100

    table filter done.

--- src/app/components/tasks-list/tasks-list.component.ts
+++ src/app/components/tasks-list/tasks-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import {animate, state, style, transition, trigger} from '@angular/animations';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { ITask } from '../../models/task.model';
 import { Store, select } from '@ngrx/store';
 import { IAppState } from '../../store/state/app.state';
@@ -13,8 +13,8 @@ import { MatSort, MatTableDataSource } from '@angular/material';
   styleUrls: ['./tasks-list.component.scss'],
   animations: [
     trigger('detailExpand', [
-      state('collapsed', style({height: '0px', minHeight: '0'})),
-      state('expanded', style({height: '*'})),
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
       transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
     ]),
   ],
@@ -24,9 +24,9 @@ export class TasksListComponent {
   tasks$: Observable<ITask[]> = this._store.pipe(select(selectTaskList));
   dataSource: MatTableDataSource<ITask>;
 
-  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
 
-  constructor(private _store: Store<IAppState>) {}
+  constructor(private _store: Store<IAppState>) { }
 
   ngOnInit() {
     this._store.dispatch(new GetTasks);
@@ -36,4 +36,11 @@ export class TasksListComponent {
     });
   }
 
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
